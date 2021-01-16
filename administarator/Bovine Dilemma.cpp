146723460ee8b2bdc75bc40df6be81f0a4f82de5
Vipul Raj <57625616+VipulRaj-123@users.noreply.github.com>
2021-01-16 12:25:21 +0530

    Rename Bovine Dilemma.cpp to administarator/Bovine Dilemma.cpp

--- administarator/Bovine Dilemma.cpp
+++ administarator/Bovine Dilemma.cpp
@@ -10,9 +10,9 @@ There are n trees growing along the river, where Argus tends Io. For this proble
 Argus will tie a rope around three of the trees, creating a triangular pasture. Its exact shape doesn't matter to Io, but its area is crucial to her. There may be many ways for Argus to arrange the fence, but only the ones which result in different areas of the pasture are interesting for Io. Calculate the number of different areas that her pasture may have. Note that the pasture must have nonzero area.
 
 Input
-The input consists of multiple test cases. The first line contains an integer t (1=t=100) — the number of test cases. Then t test cases follow, each one is described in two lines.
+The input consists of multiple test cases. The first line contains an integer t (1=t=100) â€” the number of test cases. Then t test cases follow, each one is described in two lines.
 
-In the first line of each test case there is a single integer n (1=n=50) denoting the number of trees growing along the river. Next line contains n distinct integers x1<x2<…<xn-1<xn (1=xi=50), the x-coordinates of trees growing along the river.
+In the first line of each test case there is a single integer n (1=n=50) denoting the number of trees growing along the river. Next line contains n distinct integers x1<x2<â€¦<xn-1<xn (1=xi=50), the x-coordinates of trees growing along the river.
 
 Output
 In a single line output an integer, the number of different nonzero areas that triangles with trees as vertices may have.
