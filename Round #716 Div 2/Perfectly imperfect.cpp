6ea5d18de9a4a6ca70fbb9277e4ea2bdd5464586
Vipul Raj <57625616+VipulRaj-123@users.noreply.github.com>
2021-06-15 19:41:23 +0530

    Rename Perfectly imperfect.cpp to Round #716 Div 2/Perfectly imperfect.cpp

--- Round #716 Div 2/Perfectly imperfect.cpp
+++ Round #716 Div 2/Perfectly imperfect.cpp
@@ -2,9 +2,9 @@
 A sequence b is a subsequence of an array a if b can be obtained from a by deleting some (possibly zero) elements.
 
 Input
-The first line contains an integer t (1=t=100) — the number of test cases. The description of the test cases follows.
-The first line of each test case contains an integer n (1=n=100) — the length of the array a.
-The second line of each test case contains n integers a1, a2, …, an (1=ai=104) — the elements of the array a.
+The first line contains an integer t (1=t=100) â€” the number of test cases. The description of the test cases follows.
+The first line of each test case contains an integer n (1=n=100) â€” the length of the array a.
+The second line of each test case contains n integers a1, a2, â€¦, an (1=ai=104) â€” the elements of the array a.
 
 Output
 If there's a subsequence of a whose product isn't a perfect square, print "YES". Otherwise, print "NO".
